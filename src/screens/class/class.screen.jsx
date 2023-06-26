9e4d3bbb0ce21078bea4789db1a1ec287d427ea2
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-06-26 22:02:19 +0100

    ao_feat: improve UI

--- src/screens/class/class.screen.jsx
+++ src/screens/class/class.screen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Pressable, View, Text } from "react-native"
+import { Pressable, View, Text, StyleSheet } from "react-native"
 import classes from "./data.json"
 import SelectDropdown from "react-native-select-dropdown"
 import { useNavigation } from "@react-navigation/native"
@@ -15,11 +15,13 @@ const ClassScreen = ({}) => {
 		})
 	}
 	return (
-		<View>
+		<View style={styles.container}>
 			<View>
 				<Text>Select your class {selectedClass}</Text>
 				<SelectDropdown
 					data={classes}
+					buttonStyle={styles.dropdown}
+					buttonTextStyle={styles.dropdownText}
 					onSelect={selectedItem => {
 						setSelectedClass(selectedItem.id)
 					}}
@@ -32,11 +34,42 @@ const ClassScreen = ({}) => {
 				/>
 			</View>
 
-			<Pressable disabled={!selectedClass} onPress={gotoStudentScreen}>
-				<Text>Continue</Text>
+			<Pressable
+				style={styles.pressable}
+				disabled={!selectedClass}
+				onPress={gotoStudentScreen}>
+				<Text style={{ color: "white" }}>Continue</Text>
 			</Pressable>
 		</View>
 	)
 }
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		background: "white",
+		padding: 20,
+	},
+	label: {},
+	dropdown: {
+		backgroundColor: "white",
+		width: "100%",
+		textAlign: "left",
+		borderRadius: 10,
+		marginTop: 10,
+	},
+	dropdownText: {
+		textAlign: "left",
+	},
+	pressable: {
+		backgroundColor: "#7864FF",
+		width: "100%",
+		marginTop: "auto",
+		padding: 20,
+		justifyContent: "center",
+		alignItems: "center",
+		marginBottom: 50,
+		borderRadius: 10,
+	},
+})
 export default ClassScreen
--- src/screens/student/student.screen.jsx
+++ src/screens/student/student.screen.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react"
-import { View, Text, Pressable, Alert } from "react-native"
+import { View, Text, Pressable, Alert, StyleSheet } from "react-native"
 import students from "./data.json"
 import { useNavigation, useRoute } from "@react-navigation/native"
 
 const StudentScreen = () => {
 	const [studentsInClass, setStudentsInClass] = useState([])
 	const [currentStudent, setCurrentStudent] = useState(0)
+	const [isLoading, setIsLoading] = useState(false)
+	const navigation = useNavigation()
 	const { params } = useRoute()
 
 	const { classId } = params
@@ -21,25 +23,46 @@ const StudentScreen = () => {
 
 	const { first_name, last_name } = studentInfo ?? {}
 
-	const markAttendance = async isPresent => {
-		await Alert.alert(isPresent ? "Present Ma" : "Absent")
-		if (currentStudent < studentsInClass.length) {
-			setCurrentStudent(() => currentStudent + 1)
-		}
+	const markAttendance = isPresent => {
+		setIsLoading(true)
+		setTimeout(async () => {
+			if (currentStudent < studentsInClass.length) {
+				await Alert.alert(isPresent ? "Present Ma" : "Absent", "", [
+					{
+						text: "Ok",
+						onPress: () => setCurrentStudent(() => currentStudent + 1),
+					},
+				])
+			} else {
+				Alert.alert("That's the last of them", "You will be redirected by home")
+				navigation.navigate("Class")
+			}
+			setIsLoading(false)
+		}, 1000)
 	}
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<View>
-				<Text>
+				<Text style={styles.headerText}>
 					{first_name} {last_name}
 				</Text>
 			</View>
-			<View>
-				<Pressable onPress={() => markAttendance(false)}>
+			<View style={styles.pressable_group}>
+				<Pressable
+					style={[styles.pressable, styles.absent, isLoading && styles.loading]}
+					onPress={() => markAttendance(false)}
+					disabled={isLoading}>
 					<Text>Absent</Text>
 				</Pressable>
-				<Pressable onPress={() => markAttendance(true)}>
+				<Pressable
+					style={[
+						styles.pressable,
+						styles.present,
+						isLoading && styles.loading,
+					]}
+					onPress={() => markAttendance(true)}
+					disabled={isLoading}>
 					<Text>Present</Text>
 				</Pressable>
 			</View>
@@ -47,4 +70,36 @@ const StudentScreen = () => {
 	)
 }
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		justifyContent: "center",
+	},
+	headerText: {
+		fontSize: 36,
+		marginBottom: 60,
+		textAlign: "center",
+	},
+	pressable_group: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	pressable: {
+		width: 150,
+		height: 150,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 100,
+	},
+	loading: {
+		opacity: 0.5,
+	},
+	absent: {
+		backgroundColor: "red",
+	},
+	present: {
+		backgroundColor: "#8CFAC8",
+	},
+})
 export default StudentScreen
